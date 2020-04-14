a095ac449325edc0c1f4268b7850db686a6a4a26
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-04-14 15:41:48 -0700

    compenents file created, NasaCard and NasaGrid built, axios call failing

---
+++ src/components/NasaCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NasaCard = props => {
+
+    return (
+    <div className="NasaCard">
+        {/* <img className="daily-img" src={props.imgUrl} alt="img-url-alt" /> */}
+        <div className="description">
+            <p></p>
+            {/* <button className="change-img" onClick={() => setImage()}>Next Image</button> */}
+        </div>
+    </div>
+    )
+};
+
+export default NasaCard;
---
+++ src/components/NasaGrid.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import NasaCard from "./NasaCard";
+
+function NasaGrid() {
+    const [nasaData, setNasaData] = useState([]);
+    const [unknown,setUnknown] = useState("stars");
+
+    useEffect(() => {
+        axios
+        .get('https://api.nasa.gov/planetary/apod')
+        .then(response => console.log(response))
+        .catch(err => console.log('Error: ', err))
+    }, []);
+
+    return (
+    <div className="container">
+        <div className="data-holder">
+
+
+        </div>
+    </div>
+
+    )};
+
+export default NasaGrid;
