ffad9473cf4b507980268e732cd5e9ec32ab1f24
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-04-14 19:57:15 -0700

    nav frame

--- src/NasaNav.js
+++ src/NasaNav.js
@@ -2,8 +2,12 @@ import React from "react";
 
 const NasaNav = () => {
    return (
-       <img src="nav" alt="nasa-logo"></img>
-       <a href="https://www.nasa.gov/"></a>
+       <img src="nav" alt="nasa-logo" />
+       <div>
+           <span>Home</span>
+           <span>Space</span>
+           <span>About</span>
+       </div>
    ) 
 
 };
